381b0ee9a69646575184b8c89f257148964617cd
deadlyparkour <deadlyparkour@gmail.com>
2026-01-30 19:12:56 +0300

    kafka init fix

--- scripts/kafka-init.sh
+++ scripts/kafka-init.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 set -eu
 
+tries=0
+max_tries=30
+
+until kafka-topics --bootstrap-server kafka:29092 --list >/dev/null 2>&1; do
+  tries=$((tries + 1))
+  if [ "$tries" -ge "$max_tries" ]; then
+    echo "kafka not ready after ${max_tries} tries" >&2
+    exit 1
+  fi
+  sleep 2
+ done
+
 kafka-topics --bootstrap-server kafka:29092 --create --if-not-exists --topic submissions --partitions 1 --replication-factor 1
 kafka-topics --bootstrap-server kafka:29092 --create --if-not-exists --topic results --partitions 1 --replication-factor 1
